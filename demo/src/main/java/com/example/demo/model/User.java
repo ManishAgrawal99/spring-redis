1f45783f88332f0f4a14bac410571c510175ee6a
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-07-29 13:05:45 +0530

    Added the User Model

--- demo/src/main/java/com/example/demo/model/User.java
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -1,5 +1,44 @@
 package com.example.demo.model;
 
+import java.io.Serializable;
+
 public class User implements Serializable {
+
+	private String id;
+	private String name;
+	private String salary;
+
+	public User(String id, String name, String salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSalary() {
+		return salary;
+	}
+
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	
+	
 
 }
