03ad16a4874ed3bcec21bec85b52896ac052e505
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-07-30 13:23:00 +0530

    Added User Repository

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import com.example.demo.model.User;
+
 @SpringBootApplication
 public class DemoApplication {
 
---
+++ demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+
+import java.util.List;
+import java.util.Map;
+
+import com.example.demo.model.User;
+
+public interface UserRepository {
+	
+	void save(User user);
+	Map<String, User> findAll();
+	User findById(String id);
+	void update(User user);
+	void delete(String id);
+}
